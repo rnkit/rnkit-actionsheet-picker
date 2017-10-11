139ef22df5f271adfcf5b085603248e1d349ba78
Stanislav Shakirov <punksta@protonmail.com>
2017-10-11 16:34:51 +0300

    fix #19
    https://github.com/rnkit/rnkit-actionsheet-picker/issues/19

--- android/app/src/main/java/io/rnkit/actionsheetpicker/ASPickerViewPackage.java
+++ android/app/src/main/java/io/rnkit/actionsheetpicker/ASPickerViewPackage.java
@@ -21,7 +21,7 @@ public class ASPickerViewPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new ASDatePickerViewModule(reactContext), new ASDataPickerViewModule(reactContext));
     }
 
-    @Override
+    //@Override deprecated in react-native 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return  Collections.emptyList();
     }
